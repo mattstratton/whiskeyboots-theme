2619478b16066fa69e57e3361394cf73cb498cae
Matt Stratton <matt.stratton@gmail.com>
2013-02-03 22:57:33 -0600

    Update page headers

--- wp-content/themes/whiskeyboots-theme/templates/page-header.php
+++ wp-content/themes/whiskeyboots-theme/templates/page-header.php
@@ -1,6 +1,8 @@
 <div class="page-header">
-  <!--<h1>
-    <?php echo roots_title(); ?>
-  </h1>
-  -->
+<div class="banner">
+	<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'featured_image' ); } ?>
+	<div class="bannertext">
+		<?php the_title();?>
+	</div>
+</div>
 </div>
